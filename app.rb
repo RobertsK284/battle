4213bf9cb09d6a9f72bcc629a2e0c76823c43b9f
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-14 15:04:01 +0000

    Viewing Hitpoints

--- app.rb
+++ app.rb
@@ -8,9 +8,15 @@ class Battle < Sinatra::Base
 
   post '/names' do
     p params
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb(:names)
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -5,6 +5,13 @@ feature 'Testing infrastructure' do
     fill_in 'player_1', with: 'Jack'
     fill_in 'player_2', with: 'Jill'
     click_button "Submit"
-    expect(page).to have_content 'Jack and Jill'
+    expect(page).to have_content "Jack v Jill"
+  end
+  scenario 'returns Jill: 100 HP' do
+    visit('/')
+    fill_in 'player_1', with: 'Jack'
+    fill_in 'player_2', with: 'Jill'
+    click_button "Submit"
+    expect(page).to have_content "Jill: 100 HP"
   end
 end
