693c29679e7d2e66bbb63ef8ad71bcf1e9f16a75
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-14 15:09:53 +0000

    Test Helpers added to spec

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,17 +1,11 @@
 feature 'Testing infrastructure' do
 
   scenario 'returns Jack and Jill' do
-    visit('/')
-    fill_in 'player_1', with: 'Jack'
-    fill_in 'player_2', with: 'Jill'
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Jack v Jill"
   end
   scenario 'returns Jill: 100 HP' do
-    visit('/')
-    fill_in 'player_1', with: 'Jack'
-    fill_in 'player_2', with: 'Jill'
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Jill: 100 HP"
   end
 end
